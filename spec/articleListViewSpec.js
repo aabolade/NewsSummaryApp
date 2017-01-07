1eba25395d27884e39eb3405b8c6fbaa21f83bdf
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-01-07 17:38:48 +0000

    returns html for list of headlines

---
+++ spec/articleListViewSpec.js
@@ -0,0 +1,28 @@
+(function takesAnArticleListUponInstantiation() {
+  var articleList = new ArticleList;
+  var articleListView = new ArticleListView(articleList);
+  assert.isTrue(articleListView.articleList === articleList)
+})();
+
+(function returnHTMLForNoArticle() {
+  var articleList = new ArticleList;
+  var articleListView = new ArticleListView(articleList);
+  assert.isTrue(articleListView.returnHTML() === "<ul></ul>" )
+})();
+
+(function returnHTMLForOneArticle() {
+  var articleList = new ArticleList;
+  articleList.createArticle("Headline");
+  var articleListView = new ArticleListView(articleList);
+  console.log(articleListView.returnHTML())
+  assert.isTrue(articleListView.returnHTML() === "<ul><li><div>Headline</div></li></ul>" )
+})();
+
+(function returnHTMLForMoreThanOneArticle() {
+  var articleList = new ArticleList;
+  articleList.createArticle("First Headline");
+  articleList.createArticle("Second Headline");
+  articleList.createArticle("Third Headline");
+  var articleListView = new ArticleListView(articleList);
+  assert.isTrue(articleListView.returnHTML() === "<ul><li><div>First Headline</div></li><li><div>Second Headline</div></li><li><div>Third Headline</div></li></ul>" )
+})();
---
+++ src/articleListView.js
@@ -0,0 +1,16 @@
+(function(exports) {
+  function ArticleListView(articleList) {
+    this.articleList = articleList;
+  }
+
+  ArticleListView.prototype.returnHTML = function() {
+    var openTag = "<ul>"
+    var contentHTML = ""
+    for(count=0;count<this.articleList.articles.length;count++) {
+      contentHTML+= "<li><div>" + this.articleList.showArticles()[count].headline + "</div></li>"
+    }
+      return openTag+= contentHTML + "</ul>"
+  }
+
+  exports.ArticleListView = ArticleListView;
+})(this);
